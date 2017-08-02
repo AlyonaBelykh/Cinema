983a8160025ac5ccafdb77c81d88ed5f793e1755
alena <imrintohsaka@gmail.com>
2017-08-02 14:04:00 +0300

    tabs with top rated movies

--- src/components/Paginate.js
+++ src/components/Paginate.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
-import { Pagination } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Pagination} from 'react-bootstrap';
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
+import '../../node_modules/react-tabs/style/react-tabs.css'
 import {api} from '../api';
 import {Popular} from './Popular';
-
 import './Paginate.css';
 
 const popularMoviePath = '/movie/popular';
+const topRated = '/movie/top_rated';
 
-export  class Paginate extends Component {
+export class Paginate extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [],
       currentPageNumber: 1,
       totalItems: 1,
-      itemsPerPage: 10
+      itemsPerPage: 10,
+      index: 0
     };
   };
 
@@ -30,8 +33,38 @@ export  class Paginate extends Component {
       });
   }
 
+  topRared(index){
+    console.log('topRared',  index)
+    if (index == 1) {
+      api(topRated)
+        .then(apiResponse => {
+        console.log(apiResponse)
+          this.setState({
+            data: apiResponse.data.results,
+            currentPageNumber: apiResponse.data.page,
+            totalItems: apiResponse.data.total_results,
+            itemsPerPage: apiResponse.data.results.length
+          });
+        })
+    }else{
+      this.handleSelect(1)
+    }
+  }
+
   handleSelect(number) {
-    api(popularMoviePath, '&page='+number)
+    api(popularMoviePath, '&page=' + number)
+      .then(apiResponse => {
+        this.setState({
+          data: apiResponse.data.results,
+          currentPageNumber: apiResponse.data.page,
+          totalItems: apiResponse.data.total_results,
+          itemsPerPage: apiResponse.data.results.length
+        });
+      });
+  }
+
+  handleSelectRated(number){
+    api(topRated, '&page=' + number)
       .then(apiResponse => {
         this.setState({
           data: apiResponse.data.results,
@@ -43,22 +76,40 @@ export  class Paginate extends Component {
   }
 
   render() {
-    let totalPages = Math.ceil(this.state.totalItems / this.state.itemsPerPage);
     return (
-      <div>
-        <Popular data={this.state.data}/>
+      <Tabs defaultIndex={0} onSelect={(index)=>this.topRared(index)}>
+        <TabList>
+          <Tab>Popular Movies</Tab>
+          <Tab>Top Rated Movies</Tab>
+        </TabList>
+        <TabPanel>
+          <Popular data={this.state.data}/>
+          <Pagination
+            first
+            last
+            next
+            prev
+            maxButtons={4}
+            bsSize="medium"
+            items={Math.ceil(this.state.totalItems / this.state.itemsPerPage)}
+            activePage={this.state.currentPageNumber}
+            onSelect={this.handleSelect.bind(this)}/>
+        </TabPanel>
 
-        <Pagination
-          first
-          last
-          next
-          prev
-          maxButtons={4}
-          bsSize="medium"
-          items={totalPages}
-          activePage={this.state.currentPageNumber}
-          onSelect={this.handleSelect.bind(this)}/>
-      </div>
+        <TabPanel>
+          <Popular data={this.state.data}/>
+          <Pagination
+            first
+            last
+            next
+            prev
+            maxButtons={4}
+            bsSize="medium"
+            items={Math.ceil(this.state.totalItems / this.state.itemsPerPage)}
+            activePage={this.state.currentPageNumber}
+            onSelect={this.handleSelectRated.bind(this)}/>
+        </TabPanel>
+      </Tabs>
     );
   }
 }
